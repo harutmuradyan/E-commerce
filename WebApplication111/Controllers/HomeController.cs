a991e1445078c9b345dd4aac40c2fa8500388235
Harut <harut7159@gmail.com>
2021-11-22 14:30:50 +0300

    first commit

---
+++ WebApplication111/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication111.Models;
+
+namespace WebApplication111.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+        public IActionResult Products()
+        {
+            return View(db.Phones.ToList());
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+    }
+}
---
+++ WebApplication111/Models/SampleData.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using WebApplication111.Models;
+
+namespace WebApplication111
+{
+    public static class SampleData
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (!context.Phones.Any())
+            {
+                context.Phones.AddRange(
+                new Phone
+                {
+                    Name = "iPhone X",
+                    Company = "Apple",
+                    Price = 600
+                },
+                new Phone
+                {
+                    Name = "Samsung Galaxy Edge",
+                    Company = "Samsung",
+                    Price = 550
+                },
+                new Phone
+                {
+                    Name = "Pixel 3",
+                    Company = "Google",
+                    Price = 500
+                },
+                new Phone
+                {
+                    Name = "Pixel 5",
+                    Company = "Google",
+                    Price = 700
+                }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
